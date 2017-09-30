a03a6f06c17008beba525fe60c3371a3354972e7
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-09-30 19:33:45 +0800

    check empty

--- lib/very_safe_rm/rm_one.rb
+++ lib/very_safe_rm/rm_one.rb
@@ -2,6 +2,13 @@ module VerySafeRm
   # TODO
   module RM
     def self.rm_one(file, args)
+      if Dir.empty? file then RM.do_rm file, ['-rfv']
+      elsif File.empty? file then RM.do_rm file, ['-fv']
+      else RM.do_rm file, ['-iv', *args]
+      end
+    end
+
+    def self.do_rm(file, args)
       `rm #{args.join ' '} #{file}`
     end
   end
