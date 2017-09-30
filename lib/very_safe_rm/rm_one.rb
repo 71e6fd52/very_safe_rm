2583648d93bf4dc5a391ca2abded98498a0d9887
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-09-30 19:34:43 +0800

    ingone nilfs

--- lib/very_safe_rm/rm_one.rb
+++ lib/very_safe_rm/rm_one.rb
@@ -2,7 +2,9 @@ module VerySafeRm
   # TODO
   module RM
     def self.rm_one(file, args)
-      if Dir.empty? file then RM.do_rm file, ['-rfv']
+      filesystem = `stat -fc %T #{file}`
+      if filesystem == 'nilfs' then RM.do_rm file, ['-rvf']
+      elsif Dir.empty? file then RM.do_rm file, ['-rfv']
       elsif File.empty? file then RM.do_rm file, ['-fv']
       else RM.do_rm file, ['-iv', *args]
       end
